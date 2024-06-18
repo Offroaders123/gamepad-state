945ff30cee87f939bbe017b8317f2cc07396a55f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-06-18 04:38:48 -0700

    This Testing
    Wanted to make sure that `this` works as expected, with the types I wrote for the API.
    
    https://www.youtube.com/playlist?list=PL82A88380D5A14EA9

--- app.ts
+++ app.ts
@@ -3,18 +3,18 @@ import { GamepadState } from "./src/index.js";
 const state = new GamepadState(0);
 console.log(state);
 
-state.addEventListener("connected", event => {
-  console.log(event);
+state.addEventListener("connected", function(event) {
+  console.log(this, event);
 });
 
-state.addEventListener("disconnected", event => {
-  console.log(event);
+state.addEventListener("disconnected", function(event) {
+  console.log(this, event);
 });
 
-state.addEventListener("startpolling", event => {
-  console.log(event);
+state.addEventListener("startpolling", function(event) {
+  console.log(this, event);
 });
 
-state.addEventListener("stoppolling", event => {
-  console.log(event);
+state.addEventListener("stoppolling", function(event) {
+  console.log(this, event);
 });
