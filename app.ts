e52f668f9cee0b2f5ead6bb393d2cf682a0d3a57
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-07-31 18:59:12 -0700

    Updates Display
    I should make a proper setup to render this, it helps make it more understandable much better.
    
    The thing here is to show when each of the controllers are updating, and that each handler is specific to that controller index. My Nimbus controller for some reason is represented by two controllers, so that's what I'm trying to debug here. In my games that use the Gamepad API, I'll have to add a controller picker for browsers like Chrome that don't handle the controller completely right. Safari seems to be okay with it though, so it must be the macOS implementation in Chromium that doesn't work completely with MFi devices.

--- app.ts
+++ app.ts
@@ -1,8 +1,12 @@
 import { GamepadState } from "./src/index.js";
 
 const running = document.querySelector<HTMLInputElement>("#running")!;
+const display0 = document.querySelector<HTMLInputElement>("#display0")!;
+const display1 = document.querySelector<HTMLInputElement>("#display1")!;
+const display2 = document.querySelector<HTMLInputElement>("#display2")!;
+const display3 = document.querySelector<HTMLInputElement>("#display3")!;
 
-for (const id of [0, 1, 2]) {
+for (const id of [0, 1, 2, 3] as const) {
 
 const state = new GamepadState(id);
 console.log(state);
@@ -17,6 +21,12 @@ state.addEventListener("disconnected", function(event) {
 
 state.addEventListener("input", function(event) {
   console.log(this, event);
+  switch (id) {
+    case 0: display0.valueAsNumber += 1; break;
+    case 1: display1.valueAsNumber += 1; break;
+    case 2: display2.valueAsNumber += 1; break;
+    case 3: display3.valueAsNumber += 1; break;
+  }
 });
 
 state.addEventListener("start", function(event) {
