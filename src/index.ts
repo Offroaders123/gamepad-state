160957bf28c5dd90f6a6b00d246db90670bec6bc
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-08-03 20:35:09 -0700

    Next Observer Demo
    The rehash concept, I started building it from scratch, this one isn't complete either. I'm looking to that of `IntersectionObserver` and `MutationObserver` and have found that the pattern for those just might be how I want to handle things here. Been looking at a bunch of StackOverflow posts around the two, TypeScript types, and usage patterns.

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,58 @@
-let running: boolean = false;
-const gamepadIndices: Set<number> = new Set();
-const observers: GamepadObserver[] = [];
-
-window.addEventListener("gamepadconnected", event => {
-  gamepadIndices.add(event.gamepad.index);
-});
-
-window.addEventListener("gamepaddisconnected", event => {
-  gamepadIndices.delete(event.gamepad.index);
-});
-
-function start(): void {
-  running = true;
-  poll();
-}
-
-function stop(): void {
-  running = false;
-}
-
-async function poll(): Promise<void> {
-  if (running === false) return;
-  await new Promise<void>(resolve => requestAnimationFrame(() => resolve()));
-
-  for (const gamepad of getGamepads()) {
-    const instances: GamepadObserver[] = observers.filter(observer => observer.index === gamepad.index);
-
-    for (const instance of instances) {
-      if (instance.gamepad?.timestamp === gamepad.timestamp) break;
-      instance.gamepad = gamepad;
-      instance.dispatchEvent(new GamepadEvent("input", { gamepad }));
-    }
-  }
-
-  await poll();
-}
-
-function getGamepads(): Gamepad[] {
-  return navigator
-    .getGamepads()
-    .filter(gamepad => gamepad !== null);
-}
-
-export type GamepadObserverCallback = (entries: GamepadObserverEntry[], observer: GamepadObserver) => void;
+export type GamepadObserverType = "connect" | "disconnect" | "input";
 
 export interface GamepadObserverEntry {
-  type: GamepadObserverEntryType;
-  index: number;
+  type: GamepadObserverType;
   gamepad: Gamepad;
 }
 
+export type GamepadObserverCallback = (entry: GamepadObserverEntry, observer: GamepadObserver) => void;
+
 export class GamepadObserver {
-  #indices: number[] = [];
+  private observed: Set<number> = new Set();
+  private controller = new AbortController();
 
   constructor(callback: GamepadObserverCallback) {
-    callback([], this);
+    const { signal } = this.controller;
+
+    window.addEventListener("gamepadconnected", event => {
+      if (this.observed.has(event.gamepad.index)) {
+        const { gamepad } = event;
+        callback({ type: "connect", gamepad }, this);
+      }
+    }, { signal });
+
+    window.addEventListener("gamepaddisconnected", event => {
+      if (this.observed.has(event.gamepad.index)) {
+        const { gamepad } = event;
+        callback({ type: "disconnect", gamepad }, this);
+      }
+    }, { signal });
   }
 
-  disconnect(): void {}
-
   observe(index: number): void {
-    if (typeof index !== "number") {
-      throw new TypeError("Expected a number as a gamepad index");
-    }
-
-    this.#indices.push(index);
+    this.observed.add(index);
   }
 
   unobserve(index: number): void {
-    if (typeof index !== "number") {
-      throw new TypeError("Expected a number as a gamepad index");
+    this.observed.delete(index);
+  }
+
+  disconnect(): void {
+    this.observed.clear();
+    this.controller.abort();
+  }
+
+  private async poll(): Promise<void> {
+    await new Promise<number>(requestAnimationFrame);
+
+    for (const gamepad of navigator.getGamepads()) {
+      if (gamepad === null || !this.observed.has(gamepad.index)) continue;
     }
+
+    await this.poll();
   }
 }
 
-export type GamepadObserverEntryType = "connected" | "disconnected" | "input" | "start" | "stop";
+const observer = new GamepadObserver((entry, observer) => {
+
+});
