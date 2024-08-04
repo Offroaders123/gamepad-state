fd5292375dc944f3cdbf9dab3f2620456b15a277
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-08-04 09:05:22 -0700

    Demo Formatting
    Just indentation!!

--- test/App.tsx
+++ test/App.tsx
@@ -8,40 +8,40 @@ export default function App() {
   const [getDisplay2, setDisplay2] = createSignal<number>(0);
   const [getDisplay3, setDisplay3] = createSignal<number>(0);
 
-const observer = new GamepadObserver((records, observer) => {
-  for (const record of records) {
-    switch (record.type) {
-      case "input": {
-        console.log(record.type.toUpperCase(), observer, record.gamepad);
-        switch (record.gamepad.index) {
-          case 0: setDisplay0(previous => previous + 1); break;
-          case 1: setDisplay1(previous => previous + 1); break;
-          case 2: setDisplay2(previous => previous + 1); break;
-          case 3: setDisplay3(previous => previous + 1); break;
+  const observer = new GamepadObserver((records, observer) => {
+    for (const record of records) {
+      switch (record.type) {
+        case "input": {
+          console.log(record.type.toUpperCase(), observer, record.gamepad);
+          switch (record.gamepad.index) {
+            case 0: setDisplay0(previous => previous + 1); break;
+            case 1: setDisplay1(previous => previous + 1); break;
+            case 2: setDisplay2(previous => previous + 1); break;
+            case 3: setDisplay3(previous => previous + 1); break;
+          }
+          return;
         }
-        return;
-      }
-      default: {
-        console.log(record.type.toUpperCase(), record.gamepad.id, observer);
-        return;
+        default: {
+          console.log(record.type.toUpperCase(), record.gamepad.id, observer);
+          return;
+        }
       }
     }
+  });
+
+  observer.onstart = () => {
+    console.log("start".toUpperCase(), observer);
+    setRunning(true);
+  };
+
+  observer.onstop = () => {
+    console.log("stop".toUpperCase(), observer);
+    setRunning(false);
+  };
+
+  for (const id of [0, 1, 2, 3] as const) {
+    observer.observe(id);
   }
-});
-
-observer.onstart = () => {
-  console.log("start".toUpperCase(), observer);
-  setRunning(true);
-};
-
-observer.onstop = () => {
-  console.log("stop".toUpperCase(), observer);
-  setRunning(false);
-};
-
-for (const id of [0, 1, 2, 3] as const) {
-  observer.observe(id);
-}
 
   return (
     <>
