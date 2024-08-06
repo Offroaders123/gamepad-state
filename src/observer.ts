e3551873516d9682f9459d32cd6583995cbf931e
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-08-06 01:59:34 -0700

    Debounce Partial Revert
    Going to start from the ground up for it, to make sure it's working properly.

--- src/observer.ts
+++ src/observer.ts
@@ -9,31 +9,14 @@ export interface GamepadRecord {
 
 export type GamepadObserverCallback = (records: GamepadRecord[], observer: GamepadObserver) => void;
 
-function debouncePool(callback: GamepadObserverCallback, observer: GamepadObserver): (record: GamepadRecord) => void {
-  let pool: GamepadRecord[] = [];
-  let id: number | null = null;
-
-  return record => {
-    if (id !== null) {
-      pool.push(record);
-      console.log(`Added to pool`, record.type, record.gamepad);
-      return;
-    }
-
-    id = requestAnimationFrame(() => {
-      callback(pool, observer);
-      pool = [];
-      id = null;
-    });
-  };
-}
-
 export class GamepadObserver {
   private state = new GamepadState();
   private observed: Set<number> = new Set();
 
   constructor(callback: GamepadObserverCallback) {
-    this.callback = debouncePool(callback, this);
+    this.callback = gamepad => {
+      callback([gamepad], this);
+    };
 
     this.state.onconnect = gamepad => {
       if (!this.observed.has(gamepad.index)) return;
@@ -55,7 +38,7 @@ export class GamepadObserver {
     }
   }
 
-  callback: ReturnType<typeof debouncePool>;
+  callback: (record: GamepadRecord) => void;
 
   onstart: typeof this.state.onstart = null;
   onstop: typeof this.state.onstop = null;
